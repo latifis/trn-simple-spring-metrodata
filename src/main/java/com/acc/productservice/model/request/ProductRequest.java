bd11c7ebdaf5f9455539057168934895b79c047f
latif <latifirfansyah@gmail.com>
2024-08-27 11:22:23 +0700

    add dto

---
+++ src/main/java/com/acc/productservice/model/request/ProductRequest.java
@@ -0,0 +1,11 @@
+package com.acc.productservice.model.request;
+
+public class ProductRequest {
+
+    private String name;
+
+    private long price;
+
+    private int quantity;
+
+}
---
+++ src/main/java/com/acc/productservice/model/response/ProductResponse.java
@@ -0,0 +1,13 @@
+package com.acc.productservice.model.response;
+
+public class ProductResponse {
+
+    private Long id;
+
+    private String name;
+
+    private long price;
+
+    private int quantity;
+
+}
